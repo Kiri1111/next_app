02a70a0e6dccb17d44bb3bfe5c6b056d9965ed75
Kiri1111 <92_medved@mail.ru>
2023-04-20 09:15:15 +0300

    feat: Example query function

--- pages/users.js
+++ pages/users.js
@@ -58,3 +58,41 @@ import {PostsAPI} from '@/features/createPost/api';
 // });
 // return createPost
 // };
+
+
+// import { useQuery, UseQueryResult } from '@tanstack/react-query';
+//
+// import { AxiosError } from 'axios';
+//
+// import { AccountAPI } from '@/features/account/api';
+// import { selectSetUser, useCreateAccountStore } from '@/features/account/store';
+// import { ProfileType } from '@/features/account/types';
+// import { useRefreshToken } from '@/features/auth/hooks/login/useMeQuery';
+// import { isoDate } from '@/common/utils/isoDate';
+//
+// export const useGetProfile = (): UseQueryResult<ProfileType> => {
+//     const setUser = useCreateAccountStore(selectSetUser);
+//     const { refetch } = useRefreshToken();
+//
+//     const getProfile = useQuery({
+//             queryFn: AccountAPI.getProfile,
+//             queryKey: ['getProfile'],
+//             select: (data: ProfileType) => ({ ...data, birthday: isoDate(data.birthday as string) }),
+//         retry: false,
+//         refetchInterval: false,
+//         refetchOnReconnect: false,
+//         refetchOnMount: false,
+//         refetchIntervalInBackground: false,
+//         onSuccess: (data) => {
+//             setUser(data);
+//         },
+//         onError: async (error: AxiosError,) => {
+//         if (error.response?.status === 401){
+//             await refetch();
+//             await getProfile.refetch()
+//         }
+//     }
+// });
+//
+//     return getProfile
+// };
