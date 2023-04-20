3c821e87309472b2938796bd27abd1de9de86775
Kiri1111 <92_medved@mail.ru>
2023-04-19 19:12:34 -1100

    feat: Example mutate function

--- pages/users.js
+++ pages/users.js
@@ -30,3 +30,31 @@ export async function getStaticProps(context) {
         props: {users},
     }
 }
+
+
+import {useMutation} from '@tanstack/react-query';
+
+import {AxiosError} from 'axios';
+
+import {useRefreshToken} from '@/features/auth/hooks/login/useMeQuery';
+import {PostsAPI} from '@/features/createPost/api';
+
+// export const useCreatePostMutation = () => {
+//     const { refetch } = useRefreshToken();
+//     const createPost = useMutation({
+//         queryKey:['createPost'],
+// mutationFn: PostsAPI.createPost,
+// onSuccess: () => {
+//     alert('Пост опубликован')
+// },
+// onError: async (error: AxiosError, data) => {
+//     alert('Кабзда')
+//     if (error.response?.status === 401){
+//         await refetch();
+//         await createPost.mutateAsync(data)
+//
+//     }
+// }
+// });
+// return createPost
+// };
